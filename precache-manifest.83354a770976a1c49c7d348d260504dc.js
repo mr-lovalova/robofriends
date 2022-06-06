26a104cd6dde3e843356bc41b53889297b685334
badmadafucka <47424670+badmadafucka@users.noreply.github.com>
2022-06-06 09:52:46 +0200

    Updates

---
+++ precache-manifest.83354a770976a1c49c7d348d260504dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "94fc91eafa583cf3039f",
+    "url": "/robofriends/static/css/main.2ad39dec.chunk.css"
+  },
+  {
+    "revision": "94fc91eafa583cf3039f",
+    "url": "/robofriends/static/js/main.94fc91ea.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "91d59bf331f634668428",
+    "url": "/robofriends/static/css/2.fbf75b38.chunk.css"
+  },
+  {
+    "revision": "91d59bf331f634668428",
+    "url": "/robofriends/static/js/2.91d59bf3.chunk.js"
+  },
+  {
+    "revision": "69d7087b8b0ba8fee442",
+    "url": "/robofriends/static/js/runtime~main.69d7087b.js"
+  },
+  {
+    "revision": "0fec5c0953906a6c20661a8f39013655",
+    "url": "/robofriends/index.html"
+  }
+];
